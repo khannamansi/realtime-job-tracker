780d3a92f53238b749431c289561041aa9153d16
khannamansi <khannamansi312@gmail.com>
2025-05-05 20:09:33 -0500

    Deleting unnecessary folders

--- multi-db-query-parser-Final/README.md
+++
@@ -1,4 +0,0 @@
-# multi-db-query-parser
-This repository consists of project for multi-db-query-parser.
-
-To run the application type in the command in terminal: pip install -r requirements.txt
--- multi-db-query-parser-Final/userinterface/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://54.147.60.120:5000/'
-export const parseQuery = (database, schema, query) => axios.post(BASE_URL+`query?database=${database}&query=${query}&schema=${schema}`);
-
